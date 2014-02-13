903ef20671f96cf4602692cd2b283f549ed2db58
Chris Maddox <tyre77@gmail.com>
2014-02-13 14:26:55 -0800

    version 0.0.3

--- lib/ultra_marathon/version.rb
+++ lib/ultra_marathon/version.rb
@@ -2,7 +2,7 @@ module UltraMarathon
   class Version
     MAJOR = 0 unless defined? MAJOR
     MINOR = 0 unless defined? MINOR
-    PATCH = 2 unless defined? PATCH
+    PATCH = 3 unless defined? PATCH
     PRE = nil unless defined? PRE
 
     class << self
