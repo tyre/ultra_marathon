5301fd4350c03cc33f8707d53248e31c9ab5b194
Chris Maddox <tyre77@gmail.com>
2014-06-20 11:46:04 -0700

    add instrumentation calculations

--- lib/ultra_marathon/instrumentation.rb
+++ lib/ultra_marathon/instrumentation.rb
@@ -10,7 +10,48 @@ module UltraMarathon
       @instrumentations ||= Hash.new
     end
 
+    def sorted_instrumentations
+      names_and_scores = instrumentations.sort_by do |name, profile|
+        profile.total_time
+      end
+    end
+
+    # Returns a tuple of [name, profile] for the profile that took the longest
+    def max
+      sorted_instrumentations.last
+    end
+
+    # Returns a tuple of [name, profile] for the profile that took the shortest
+    def min
+      sorted_instrumentations.first
+    end
+
+    # Returns the mean time for profiles
+    def mean_time
+      @mean ||= total_times.reduce(0, :+) / instrumentations.size
+    end
+
+    # Returns a tuple of [name, profile] for the profile in the middle of the pack
+    def median
+      @median ||= sorted_instrumentations[instrumentations.size / 2]
+    end
+
+    # Returns the standard deviation from the mean
+    # Please forgive me Mr. Brooks, I had to Google it
+    def standard_deviation
+      @standard_deviation ||= begin
+        sum_of_deviation_squares = total_times.reduce(0) do |sum, total_time|
+          sum + (mean_time - total_time) ** 2
+        end
+        Math.sqrt(sum_of_deviation_squares / instrumentations.size)
+      end
+    end
+
     private
+
+    def total_times
+      instrumentations.values.map(&:total_time)
+    end
 
     ## Private Instance Methods
 
--- spec/ultra_marathon/instrumentation_spec.rb
+++ spec/ultra_marathon/instrumentation_spec.rb
@@ -28,4 +28,52 @@ describe UltraMarathon::Instrumentation do
       profile.should be_an UltraMarathon::Instrumentation::Profile
     end
   end
+
+  describe 'calculation methods' do
+    let(:min_profile) { double(:min_profile, total_time: 1) }
+    let(:max_profile) { double(:max_profile, total_time: 247) }
+    let(:mediocre_profile) { double(:mediocre_profile, total_time: 13.5) }
+    let(:profiles) { [min_profile, max_profile, mediocre_profile] }
+
+    before(:each) do
+      test_instance.stub(:instrumentations) do
+        {
+          min_profile: min_profile,
+          max_profile: max_profile,
+          mediocre_profile: mediocre_profile
+        }
+      end
+    end
+
+    describe '#min' do
+      it 'should return the name and profile of the min' do
+        test_instance.min.should eq [:min_profile, min_profile]
+      end
+    end
+
+    describe '#max' do
+      it 'should return the name and profile of the max' do
+        test_instance.max.should eq [:max_profile, max_profile]
+      end
+    end
+
+    describe '#median' do
+      it 'should return the name and profile of the median' do
+        test_instance.median.should eq [:mediocre_profile, mediocre_profile]
+      end
+    end
+
+    describe '#mean_time' do
+      it 'should return the mean time for all profiles' do
+        total_time = profiles.reduce(0) { |sum, profile| sum + profile.total_time }
+        test_instance.mean_time.should eq (total_time/profiles.size)
+      end
+    end
+
+    describe '#standard_deviation' do
+      it 'should return the standard deviation of the total times' do
+        test_instance.standard_deviation.round(2).should eq 113.13
+      end
+    end
+  end
 end
